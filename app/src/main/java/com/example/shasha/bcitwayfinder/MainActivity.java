49be6a1b661ebf67315144951ae143f265bcbde2
ericjsy <ericjsy604@gmail.com>
2018-08-02 22:29:56 -0700

    Connect app to Firebase.

--- app/src/main/java/com/example/shasha/bcitwayfinder/MainActivity.java
+++ app/src/main/java/com/example/shasha/bcitwayfinder/MainActivity.java
@@ -10,6 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
