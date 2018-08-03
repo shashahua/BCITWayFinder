88cc4f120683725a21529dfa2e5086e326cc31b8
Gtsui091 <Tsui.gordon.9@gmail.com>
2018-08-02 20:09:44 -0700

    Set up MapFragment using mapsActivity as base

--- app/src/main/java/com/example/shasha/bcitwayfinder/MapFragment.java
+++ app/src/main/java/com/example/shasha/bcitwayfinder/MapFragment.java
@@ -8,11 +8,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MapFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap map;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View v =  inflater.inflate(R.layout.fragment_map, container, false);
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map1);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng homebcit = new LatLng(49.25, -123.00);
+        MarkerOptions option = new MarkerOptions();
+        option.position(homebcit).title("BCIT");
+        map.addMarker(option);
+        map.moveCamera(CameraUpdateFactory.newLatLng(homebcit));
+
     }
 }
